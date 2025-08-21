3d2ab0505511c78bc809955fb8994da7b5634c79
jozzzof <lowkeyusef420@gmail.com>
2025-08-21 15:50:59 +0100

    Updates: Added a loading text so that the api always fetches data, added background color green to correct answer and background color red to wrong answer. Problems: The score display is still incorrect. The background color for the buttons persists even if the next question button is clicked.

--- script.js
+++ script.js
@@ -1,38 +1,55 @@
-fetch("https://opentdb.com/api.php?amount=1&difficulty=medium&type=boolean")
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    data.results.forEach((q) => {
-      const quizCategory = document.getElementById("question-category");
-      const quizQuestion = document.getElementById("question-text");
-      const trueButton = document.getElementById("true-button");
-      const falseButton = document.getElementById("false-button");
-      const questionAnswer = document.getElementById("question-answer");
+function playGame() {
+  function fetchCondition() {
+    fetch("https://opentdb.com/api.php?amount=1&difficulty=medium&type=boolean")
+      .then((response) => {
+        if (!response.ok) {
+          const quizCategory = document.getElementById("question-category");
+          const quizQuestion = document.getElementById("question-text");
+          const questionAnswer = document.getElementById("question-answer");
+          let score = document.getElementById("score");
+          const buttonContainer = document.querySelectorAll(".answer-button");
 
-      quizCategory.innerHTML = `Category: ${q.category}`;
-      quizQuestion.innerHTML = q.question;
-
-      trueButton.addEventListener("click", () => {
-        if (q.correct_answer == "True") {
-          questionAnswer.innerHTML = "You got a correct answer!";
-        } else {
-          questionAnswer.innerHTML = "You got a wrong answer!";
+          quizCategory.innerHTML = `Category: Loading, Please Wait...`;
+          quizQuestion.innerHTML = `Question: Loading, Please Wait...`;
+          fetchCondition();
         }
-      });
+        return response.json();
+      })
+      .then((data) => {
+        data.results.forEach((q) => {
+          const quizCategory = document.getElementById("question-category");
+          const quizQuestion = document.getElementById("question-text");
+          const questionAnswer = document.getElementById("question-answer");
+          let score = document.getElementById("score");
+          const buttonContainer = document.querySelectorAll(".answer-button");
 
-      falseButton.addEventListener("click", () => {
-        if (q.correct_answer == "False") {
-          questionAnswer.innerHTML = "You got a correct answer!";
-        } else {
-          questionAnswer.innerHTML = "You got a wrong answer!";
-        }
+          quizCategory.innerHTML = `Category: ${q.category}`;
+          quizQuestion.innerHTML = `Question: ${q.question}`;
+
+          buttonContainer.forEach((button) => {
+            button.addEventListener("click", () => {
+              console.log(button.textContent);
+              if (q.correct_answer == button.textContent) {
+                button.classList.add("correct-button");
+                questionAnswer.innerHTML = "You got a correct answer!";
+                score.innerHTML = Number(score.textContent) + 1;
+              } else {
+                button.classList.add("wrong-button");
+                questionAnswer.innerHTML = "You got a wrong answer!";
+                score.innerHTML = Number(score.textContent) - 1;
+              }
+            });
+          });
+          nextQuestion();
+        });
       });
-    });
-  })
-  .catch((error) => {
-    console.error("Error fetching data", error);
+  }
+  fetchCondition();
+}
+
+function nextQuestion() {
+  const nextQuestionButton = document.getElementById("next-question-button");
+  nextQuestionButton.addEventListener("click", () => {
+    playGame();
   });
+}
